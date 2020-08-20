d2d98c71feaf54c4ca2523a64e66006fa32770a3
Ayodeji Amosun <ayodeji.amosun04@gmail.com>
2020-08-20 10:37:23 -0500

    ?

--- client/src/pages/Members.js
+++ client/src/pages/Members.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { useStoreContext } from '../utils/GlobalStore';
+import { Box } from "rebass";
 
 function Members(){
     const [state] = useStoreContext();
     const {email} = state;
-    return <div className="container">
-    <div className="row">
-      <div className="col-md-6 col-md-offset-3">
-        <h2>Welcome {email}</h2>
+    return <div>
+      <div className="container">
+      <div className="row">
+        <div className="col-md-6 col-md-offset-3">
+          <h2>Welcome {email}</h2>
+        </div>
       </div>
     </div>
   </div>
