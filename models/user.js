abbcad09a1168fbe7dfe37e6a24c24508b927923
MMMarz009 <mar0099887@gmail.com>
2020-08-20 10:13:49 -0500

    Added Nickname Trait to User

--- models/user.js
+++ models/user.js
@@ -16,6 +16,15 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    //Added nickname Trait to User
+    nickName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len:[1]
+      }
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
