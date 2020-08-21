323a714d6090e6ad083443b9520df4b080a69690
MMMarz009 <mar0099887@gmail.com>
2020-08-21 11:20:49 -0500

    Where's the NOTION ON THIS FOR EASY REF I DON'T WANNA DO THIS EVERY TIME

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('*',(req,res) =>{
+app.get('/',(req,res) =>{
   res.sendFile(path.join("public", "index.html"));
 })
 
