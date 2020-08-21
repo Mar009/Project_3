698c43d38e23ab0e49cf91d395776209d6537608
MMMarz009 <mar0099887@gmail.com>
2020-08-21 11:04:27 -0500

    added nickname to api, will solve later. Made a build here to test deployment but idk man

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -41,7 +41,8 @@ module.exports = function(app) {
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
         email: req.user.email,
-        id: req.user.id
+        id: req.user.id,
+        nickname: req.user.nickname
       });
     }
   });
