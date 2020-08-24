9e650743ca8a2452ea036a93b78705f4e407ac12
Ayodeji Amosun <ayodeji.amosun04@gmail.com>
2020-08-24 14:43:29 -0500

    figured out how to get data from database onto the history page!

--- client/src/pages/HistoryPg.js
+++ client/src/pages/HistoryPg.js
@@ -1,39 +1,45 @@
 import React from "react";
 import { Heading } from "rebass";
 import Table from "../components/HistoryLog";
-import axios from "axios";
 
+class TablePage extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            historyLogs: []
+        }
 
-// var db = require("../../../models/symptoms");
-
-function TablePage() {
-    // axios.get("/api/symptom_data", function(req, res) {db.Symptoms.findAll({ where: {name: "fever"}}).then(dbSymptoms => {
-    //     console.log(res.JSON({dbSymptoms})
-    //     )})})
-
-        // dbSymptoms.symptom.map(symptom =>
-        //     <Table symptom={symptom.name} dateLogged={symptom.data} />
-        // )
+    }
     
+    componentDidMount() {
+        fetch("/api/symptom_data")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                this.setState({
+                    historyLogs: data
+                })
+            })
+    }
 
-    return (
-        <div>
-            <Heading
-            sx={{
-                m: 80,
-                textAlign: "center"
-            }}>YOUR HISTORY!</Heading>
-            {/* <Table 
-            symptom = "placeholder symptom"
-            dateLogged = "placeholder date"
-            />
-            <Table 
-            symptom = "placeholder symptom2"
-            dateLogged = "placeholder date2"
-            /> */}
-            {/* {symptomLog} */}
-        </div>
-    )
+
+    render() {
+        console.log(this.state)
+        const yourHistory = this.state.historyLogs.map(symptom => <Table symptom={symptom.name} dateLogged={symptom.createdAt} />)
+        
+        return (
+            <div>
+                <Heading
+                sx={{
+                    m: 80,
+                    textAlign: "center"
+                }}>YOUR HISTORY!</Heading>
+                
+                {yourHistory}
+            </div>
+        )
+    }
+    
 }
 
 export default TablePage;
